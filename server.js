0d07cf3a8d35eeb6990bea198459acfdea1febf3
opazisimo <suopazo@4talent.cl>
2017-02-15 02:22:08 -0300

    pusheando a heroku 2

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var application = express();
 application.set('port',(process.env.PORT || 8086));
 application.use(express.static(__dirname + '/dist'));
 
+application.get('/', function(req,res){
+  res.render('index');
+})
+
+application.get('/jugadores', function(req,res){
+  res.render('index');
+})
+
 application.listen(application.get('port'), function(){
   console.log('Server en ', application.get('port'));
 });
